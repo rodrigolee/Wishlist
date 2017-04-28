bd74c7bef7f35e9026bcfcc4e28b2808959ee217
Rodrigo Lee <rodrigolee@Rodrigos-MacBook-Pro.local>
2017-04-27 21:05:34 -0700

    Initial Commit

---
+++ Friends Bucket List/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Friends Bucket List
+//
+//  Created by Rodrigo Lee on 4/27/17.
+//  Copyright © 2017 Rodrigo Lee. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
